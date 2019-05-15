7fdcad39a0a76d032277f77581422ffbf3dafeae
Aleksey Olshevski <alexeyolshevsky@coherentsolutions.com>
2019-05-15 10:28:38 +0300

    Join dataframes

--- src/main/scala/Airlines.scala
+++ src/main/scala/Airlines.scala
@@ -1,5 +1,6 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{Column, DataFrame, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 
@@ -13,6 +14,54 @@ object Airlines {
     df
   }
 
+  def getDfSummary(df: DataFrame) = {
+    val aircraftSummary = df.describe()
+    println(aircraftSummary.show())
+  }
+
+  def simplifyAircraftDF(df: DataFrame) : DataFrame = {
+    val header = df.first()
+    var f_df = df.filter(row => row != header)
+    val aircraftColToRemove = Seq("status", "seatconfiguration", "notes")
+
+    f_df = f_df.select(f_df.columns.filter(c => !aircraftColToRemove.contains(c)).map(c => new Column(c)): _*)
+    f_df
+  }
+
+  def joinDFs(airlines:DataFrame, aircraft:DataFrame, aircraftType: DataFrame) : DataFrame = {
+    val dfAsAirlines = airlines.as("airlines")
+    val dfAsAircraft = aircraft.as("aircraft")
+    val dfAsAircraftType = aircraftType.as("aircraftType")
+
+    val joinType = "left_semi"
+
+    val joinedAircraftType = dfAsAircraft.join(dfAsAircraftType,
+      col("aircraft.typecode") === col("aircraftType.Designator"),
+      joinType)
+
+    val dfAsJoinedAircraft = joinedAircraftType.as("joinedAircraft")
+    println("Joined aircrafts with aircraft types")
+    println(joinedAircraftType.show())
+
+    println(s"Count of partitions in df_asJoinedAircrafts: ${dfAsJoinedAircraft.rdd.getNumPartitions}")
+    println(s"Count of partitions in df_asHoursAirlines: ${dfAsJoinedAircraft.rdd.getNumPartitions}")
+
+    val joinedAirlinesAircraft = dfAsAirlines.join(dfAsJoinedAircraft,
+      col("joinedAircraft.icao24") === col("airlines.icao24"),
+      "left_outer")
+      .drop(col("joinedAircraft.icao24"))
+
+    println(joinedAirlinesAircraft.explain())
+    println("Joined stream airplanes with joined aircraft with aircraft types")
+    println(joinedAirlinesAircraft.orderBy("time_position").show())
+
+    println(s"Count of partitions in joined df: ${joinedAirlinesAircraft.rdd.getNumPartitions}")
+    val outputDf = joinedAirlinesAircraft.coalesce(5)
+    println(s"Count of partitions after coalesce: ${outputDf.rdd.getNumPartitions}")
+
+    outputDf
+  }
+
 
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
@@ -22,19 +71,26 @@ object Airlines {
 
     val folders = List.tabulate(4)(i => s"D:\\Training\\Opensky\\data\\data_0${i}h__20_04_2019")
 
-    var aircraftsDF = readDF("D:\\Python\\Work projects\\parsing_linkedin\\collect_data\\aircraftDatabase.csv",
+    var aircraftDF = readDF("D:\\Python\\Work projects\\parsing_linkedin\\collect_data\\aircraftDatabase.csv",
       "csv", sqlContext)
-    val aircraftsTypesDF = readDF("D:\\Python\\Work projects\\parsing_linkedin\\collect_data\\aircraftTypes.csv",
+    val aircraftTypesDF = readDF("D:\\Python\\Work projects\\parsing_linkedin\\collect_data\\aircraftTypes.csv",
       "csv", sqlContext)
 
-    val header = aircraftsDF.first()
-    aircraftsDF = aircraftsDF.filter(row => row != header)
+    aircraftDF = simplifyAircraftDF(aircraftDF)
 
-    println(aircraftsDF.show())
-    println(aircraftsTypesDF.show())
+    println(aircraftDF.show())
+    println(aircraftTypesDF.show())
+    println(s"Count of aircraft DB: ${aircraftDF.count()}")
 
-    println(s"Count of aircraft DB: ${aircraftsDF.count()}")
+    val hoursAirlinesDF = sqlContext.read
+      .format("csv")
+      .option("header", "true")
+      .csv(folders: _*)
+    println(hoursAirlinesDF.show())
+    println(s"Count of airlines: ${hoursAirlinesDF.count()}")
 
+    val smpHoursAirlinesDF = hoursAirlinesDF.sample(withReplacement = true,0.1, seed = 1)
 
+    joinDFs(smpHoursAirlinesDF, aircraftDF, aircraftTypesDF)
   }
 }
